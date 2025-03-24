76c2bc9c35f637948a35cbc70304cca04c3191a1
Samuel Teixeira Dias <samuelteixeiradias008@gmail.com>
2025-03-23 23:01:44 -0300

    fix(Main) : fix main

--- src/main/java/com/github/srteixeiradias/explode_excel_formatter/ExplodeExcelFormatterApplication.java
+++ src/main/java/com/github/srteixeiradias/explode_excel_formatter/ExplodeExcelFormatterApplication.java
@@ -1,13 +1,40 @@
 package com.github.srteixeiradias.explode_excel_formatter;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.github.srteixeiradias.explode_excel_formatter.application.ExcelProcessorService;
+import com.github.srteixeiradias.explode_excel_formatter.infraestructure.ExcelReader;
+import com.github.srteixeiradias.explode_excel_formatter.infraestructure.ExcelWriter;
 
-@SpringBootApplication
+import java.io.File;
+import java.util.Scanner;
+
 public class ExplodeExcelFormatterApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ExplodeExcelFormatterApplication.class, args);
-	}
+		Scanner scanner = new Scanner(System.in);
 
+		System.out.println("Digite o caminho do arquivo Excel de entrada:");
+		String inputPath = scanner.nextLine();
+		File inputFile = new File(inputPath);
+
+		if (!inputFile.exists() || !inputFile.isFile()) {
+			System.err.println("Arquivo de entrada não encontrado. Verifique o caminho e tente novamente.");
+			return;
+		}
+
+		System.out.println("Digite o caminho onde deseja salvar o arquivo Excel processado:");
+		String outputPath = scanner.nextLine();
+		File outputFile = new File(outputPath);
+
+		
+		ExcelReader excelReader = new ExcelReader();
+		ExcelWriter excelWriter = new ExcelWriter();
+		ExcelProcessorService excelProcessorService = new ExcelProcessorService(excelReader, excelWriter);
+
+		try {
+			excelProcessorService.process(inputFile, outputFile);
+			System.out.println("Processamento concluído com sucesso! Arquivo salvo em: " + outputPath);
+		} catch (Exception e) {
+			System.err.println("Erro ao processar o arquivo: " + e.getMessage());
+		}
+	}
 }
