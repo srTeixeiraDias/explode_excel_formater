ae8e48396ee8667cd56fb44a1dc6705366acd5ec
Samuel Teixeira Dias <samuelteixeiradias008@gmail.com>
2025-03-23 22:10:22 -0300

    feat(init) : initial commit

---
+++ src/main/java/com/github/srteixeiradias/explode_excel_formatter/ExplodeExcelFormatterApplication.java
@@ -0,0 +1,13 @@
+package com.github.srteixeiradias.explode_excel_formatter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplodeExcelFormatterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplodeExcelFormatterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/srteixeiradias/explode_excel_formatter/ExplodeExcelFormatterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.srteixeiradias.explode_excel_formatter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplodeExcelFormatterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
